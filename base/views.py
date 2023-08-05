de7500146160a1c1e71acc2b73a364c0128bc0a8
mhergrigo <grigomherml@gmail.com>
2023-08-05 04:45:41 -0700

    add update and delete

--- base/views.py
+++ base/views.py
@@ -1,24 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
 
-rooms = [
-    {'id':1,'name':'Lets learn python!'},
-    {'id':2,'name':'Design with me'},
-    {'id':3,'name':'Frontend Developers'},
-]
+# rooms = [
+#     {'id':1,'name':'Lets learn python!'},
+#     {'id':2,'name':'Design with me'},
+#     {'id':3,'name':'Frontend Developers'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # request.POST.get('name')
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance=room)
+    
+    if request == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
